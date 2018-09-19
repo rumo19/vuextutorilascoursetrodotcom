9cf3f7f0a3e514eae3562ac9a69edf656ac31eac
Md Kamrul Hasan <khasan.cse19@gmail.com>
2018-09-19 13:38:49 +0200

    vuex tutorilas

---
+++ src/store.js
@@ -0,0 +1,49 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    title: 'Vuex Tutorial',
+    links: [
+        'http://google.com',
+        'http://coursetro.com',
+        'http://youtube.com'
+      ]
+  },
+  getters: {
+    countLinks: state => {
+      return state.links.length
+    }
+  },
+  
+  mutations: {
+    ADD_LINK: (state, link) => {
+        state.links.push(link)
+      },
+      REMOVE_LINK: (state, link) => {        // Add this:
+        state.links.splice(link, 1)
+      },
+      REMOVE_ALL: (state) => {                     // Add this
+        state.links = []
+      }
+
+  },
+  actions: {
+
+    removeLink: (context, link) => {       // Add this:
+        context.commit("REMOVE_LINK", link)
+      },
+      removeAll ({commit}) {                       // Add this
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            commit('REMOVE_ALL')
+            resolve()
+          }, 1500)
+        })
+      }
+    
+
+  }
+})
